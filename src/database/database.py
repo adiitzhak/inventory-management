618f50aa1318c0a913b36f9d721af232919d864d
adiItzhak1 <adidoh02@gmail.com>
2025-11-08 19:57:13 +0200

    add constants for database

--- src/database/database.py
+++ src/database/database.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-
+from src.database.constants.constants import DATABASE_CONNECTION
 
 class Database:
     _instance = None
     def __new__(cls):
         if cls._instance is None:
             cls._instance =super().__new__(cls)
-            cls._instance.client = MongoClient("mongodb://localhost:27017/")
+            cls._instance.client = MongoClient(DATABASE_CONNECTION)
             cls._instance.db = cls._instance.client["inventory"]
             cls._instance.collection = cls._instance.db["products"]
         return cls._instance
